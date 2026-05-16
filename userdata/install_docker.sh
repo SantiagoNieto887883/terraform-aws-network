eaba343bebdbe99aee68f25d70e138aa927ddf8b
SantiagoNieto887883 <sant887883@gmail.com>
2026-05-16 14:02:29 -0500

    userdata to ec2 autobuild app

---
+++ userdata/install_docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+yum update -y
+
+amazon-linux-extras install docker -y
+
+systemctl start docker
+systemctl enable docker
+
+docker run -d -p 80:5000 dockersant887883/flask-devops-app:v1
