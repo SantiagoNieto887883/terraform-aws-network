18a7107355d1ac9f7359750b44ad305436f974c9
SantiagoNieto887883 <sant887883@gmail.com>
2026-05-18 22:30:15 -0500

    git add OIDC to github

---
+++ IAC-FULL-COST/modules/github_oidc/outputs.tf
@@ -0,0 +1,9 @@
+# modules/github_oidc/outputs.tf
+
+output "role_arn" {
+  value = aws_iam_role.github_actions.arn
+}
+
+output "oidc_provider_arn" {
+  value = aws_iam_openid_connect_provider.github.arn
+}
